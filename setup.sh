b4db4f9c26444b2e446a1cc31448c4ec0b4b7157
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-02-24 13:28:40 +0000

    Modified wago telegraf

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-read -p "Solis/Growatt/Huawei? [S/G/H]: " x
+read -p "Wago/Growatt/Huawei? [W/G/H]: " x
 
-if [[ ! $x =~ ^(S|s|G|g|H|h)$ ]]; then
-    echo "invalid option"
-    exit 1
+if [[ ! $x =~ ^(w|W|G|g|H|h)$ ]]; then
+  echo "invalid option"
+  exit 1
 fi
 
 echo "Installing telegraf..."
 sleep 2
 curl -fsSL https://repos.influxdata.com/influxdata-archive_compat.key -o /etc/apt/keyrings/influxdata-archive_compat.key
-echo "deb [signed-by=/etc/apt/keyrings/influxdata-archive_compat.key] https://repos.influxdata.com/debian stable main" | tee /etc/apt/sources.list.d/influxdata.list 
+echo "deb [signed-by=/etc/apt/keyrings/influxdata-archive_compat.key] https://repos.influxdata.com/debian stable main" | tee /etc/apt/sources.list.d/influxdata.list
 apt update
 apt -y install telegraf
 
@@ -20,20 +20,19 @@ usermod -aG dialout telegraf
 sudo systemctl enable telegraf
 
 echo "Creating telegraf.conf..."
-if [[ "$x" == "s" || "$x" == "S" ]]; then
-    sudo cp ./solis_telegraf.conf /etc/telegraf/telegraf.conf
-    echo "Enabling the CAN HAT..."
-    sleep 2
-    sudo bash -c 'echo "enable_uart=1" >> /boot/firmware/config.txt'
+if [[ "$x" == "w" || "$x" == "W" ]]; then
+  sudo cp ./wago_telegraf.conf /etc/telegraf/telegraf.conf
+  echo "Enabling the CAN HAT..."
+  sleep 2
+  sudo bash -c 'echo "enable_uart=1" >> /boot/firmware/config.txt'
 elif [[ "$x" == "g" || "$x" == "G" ]]; then
-    sudo cp ./growatt_telegraf.conf /etc/telegraf/telegraf.conf
-    echo "Enabling the CAN HAT..."
-    sleep 2
-    sudo bash -c 'echo "enable_uart=1" >> /boot/firmware/config.txt'
+  sudo cp ./growatt_telegraf.conf /etc/telegraf/telegraf.conf
+  echo "Enabling the CAN HAT..."
+  sleep 2
+  sudo bash -c 'echo "enable_uart=1" >> /boot/firmware/config.txt'
 elif [[ "$x" == "h" || "$x" == "H" ]]; then
-    sudo cp ./huawei_telegraf.conf /etc/telegraf/telegraf.conf
+  sudo cp ./huawei_telegraf.conf /etc/telegraf/telegraf.conf
 fi
-
 
 loginctl enable-linger
 
@@ -43,7 +42,7 @@ sudo bash -c 'echo "211.24.220.161  723tgfhf7917f32.time.com.my" >> /etc/hosts'
 
 echo "Installing rpi-clone..."
 sleep 2
-curl https://raw.githubusercontent.com/geerlingguy/rpi-clone/master/install | sudo bash 
+curl https://raw.githubusercontent.com/geerlingguy/rpi-clone/master/install | sudo bash
 
 echo "Rebooting..."
 sleep 2
